0f55ecf943a7db18ad7ec928c54c6cbf780f4792
Matías Castro <mzkzrrw@gmail.com>
2022-11-06 09:24:55 -0300

    fix

--- resources/views/admin/layout/logo.blade.php
+++ resources/views/admin/layout/logo.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ url('admin') }}" class="navbar-brand">
+<a href="{{ url('admin') }}" class="navbar-brand" style="margin-right:0">
     {{-- You may use plain text as a logo instead of image --}}
     <img src="/images/lupulo.png" alt="La Buena Birra" style="max-height:40px">
     {{--Text Logo--}}
