0f57faae1f3fb27d425b1545e8aae6b9ef857f8f
Matías Castro <mzkzrrw@gmail.com>
2022-11-06 21:47:01 -0300

    compilacion

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,14 +1,19 @@
+<style>
+    .items_navbar i {
+        color: white !important;
+    }
+</style>
 <div class="sidebar">
-    <nav class="sidebar-nav">
-        <ul class="nav">
+    <nav class="sidebar-nav" style="background-color:#23540e">
+        <ul class="nav items_navbar">
             <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.content') }}</li>
             <li class="nav-item"><a class="nav-link" href="{{ url('admin/provincia') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.provincium.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/localidads') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.localidad.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/productors') }}"><i class="nav-icon icon-energy"></i> {{ trans('admin.productor.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/cervezas') }}"><i class="nav-icon icon-diamond"></i> {{ trans('admin.cerveza.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/cerveceria') }}"><i class="nav-icon icon-flag"></i> {{ trans('admin.cervecerium.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/punto-venta') }}"><i class="nav-icon icon-ghost"></i> {{ trans('admin.punto-ventum.title') }}</a></li>
-           {{-- Do not delete me :) I'm used for auto-generation menu items --}}
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/localidads') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.localidad.title') }}</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/productors') }}"><i class="nav-icon icon-energy"></i> {{ trans('admin.productor.title') }}</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/cervezas') }}"><i class="nav-icon icon-diamond"></i> {{ trans('admin.cerveza.title') }}</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/cerveceria') }}"><i class="nav-icon icon-flag"></i> {{ trans('admin.cervecerium.title') }}</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/punto-venta') }}"><i class="nav-icon icon-ghost"></i> {{ trans('admin.punto-ventum.title') }}</a></li>
+            {{-- Do not delete me :) I'm used for auto-generation menu items --}}
 
             <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.settings') }}</li>
             <li class="nav-item"><a class="nav-link" href="{{ url('admin/admin-users') }}"><i class="nav-icon icon-user"></i> {{ __('Manage access') }}</a></li>
@@ -17,5 +22,5 @@
             {{--<li class="nav-item"><a class="nav-link" href="{{ url('admin/configuration') }}"><i class="nav-icon icon-settings"></i> {{ __('Configuration') }}</a></li>--}}
         </ul>
     </nav>
-    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+    <button class="sidebar-minimizer brand-minimizer" type="button" style="background-color:#133306"></button>
 </div>
