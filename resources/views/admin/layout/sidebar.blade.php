5702866031c93cd1655f676b90ff646dcc963f35
Matías Castro <mzkzrrw@gmail.com>
2022-11-07 00:19:25 -0300

    update translations

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -6,7 +6,7 @@
 <div class="sidebar">
     <nav class="sidebar-nav" style="background-color:#23540e">
         <ul class="nav items_navbar">
-            <li class="nav-title"><!--{{ trans('brackets/admin-ui::admin.sidebar.content') }}-->Contenido</li>
+            <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.content') }}</li>
             <li class="nav-item"><a class="nav-link" href="{{ url('admin/provincia') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.provincium.title') }}</a></li>
             <li class="nav-item"><a class="nav-link" href="{{ url('admin/localidads') }}"><i class="nav-icon icon-compass"></i> {{ trans('admin.localidad.title') }}</a></li>
             <li class="nav-item"><a class="nav-link" href="{{ url('admin/productors') }}"><i class="nav-icon icon-energy"></i> {{ trans('admin.productor.title') }}</a></li>
@@ -15,11 +15,11 @@
             <li class="nav-item"><a class="nav-link" href="{{ url('admin/punto-venta') }}"><i class="nav-icon icon-ghost"></i> {{ trans('admin.punto-ventum.title') }}</a></li>
             {{-- Do not delete me :) I'm used for auto-generation menu items --}}
 
-            <li class="nav-title"><!--{{ trans('brackets/admin-ui::admin.sidebar.settings') }}-->Configuraciones</li>
-            <li class="nav-item"><a class="nav-link" href="{{ url('admin/admin-users') }}"><i class="nav-icon icon-user"></i> {{ __('Administrar acceso') }}</a></li>
-            <li class="nav-item"><a class="nav-link" href="{{ url('admin/translations') }}"><i class="nav-icon icon-location-pin"></i> {{ __('Traducciones') }}</a></li>
+            <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.settings') }}</li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/admin-users') }}"><i class="nav-icon icon-user"></i> {{ __('Manage access') }}</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('admin/translations') }}"><i class="nav-icon icon-location-pin"></i> {{ __('Translations') }}</a></li>
             {{-- Do not delete me :) I'm also used for auto-generation menu items --}}
-            {{--<li class="nav-item"><a class="nav-link" href="{{ url('admin/configuration') }}"><i class="nav-icon icon-settings"></i> {{ __('Configuración') }}</a></li>--}}
+            {{--<li class="nav-item"><a class="nav-link" href="{{ url('admin/configuration') }}"><i class="nav-icon icon-settings"></i> {{ __('Configuration') }}</a></li>--}}
         </ul>
     </nav>
     <button class="sidebar-minimizer brand-minimizer" type="button" style="background-color:#133306"></button>
